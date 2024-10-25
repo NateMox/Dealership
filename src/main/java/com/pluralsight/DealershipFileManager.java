fe75e375345fbf76717d6da76acc2946706d892d
Nate Mox <nathanmoxam2@gmail.com>
2024-10-25 14:54:01 -0700

    initial commit

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,34 @@
 package com.pluralsight;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class DealershipFileManager {
+
+
+    public Dealership getDealership() {
+        Dealership dealership = null;
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/inventory.csv"))) {
+            String line = br.readLine();
+            if (line != null) {
+                String[] dealershipData = line.split("\\|");
+                String name = dealershipData[0];
+                String address = dealershipData[1];
+                String phone = dealershipData[2];
+
+
+                dealership = new Dealership(name, address, phone);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dealership;
+    }
+public void saveDealership(Dealership dealership) {
+ }
 }
+
+
+
+
